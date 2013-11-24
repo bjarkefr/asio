87166ef3e8bd3306d4a68ff949e4892e7fe8f7d7
Bjarke <bjarke@none.none>
2013-11-24 11:32:11 +0100

    Compiles and runs - but doesn't do anything...

--- src/server/main.cc
+++ src/server/main.cc
@@ -8,6 +8,11 @@
 #include <sstream>
 #include <memory>
 #include <algorithm>
+#ifdef __MINGW32__
+#include <winsock2.h>
+#else
+#include <arpa/inet.h>
+#endif
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <boost/bind.hpp>
@@ -16,10 +21,10 @@
 #include <boost/assert.hpp>
 #include "../contract/SessionRequest.pb.h"
 
-uint32_t htonl(uint32_t hostlong);
-uint16_t htons(uint16_t hostshort);
-uint32_t ntohl(uint32_t netlong);
-uint16_t ntohs(uint16_t netshort);
+//uint32_t htonl(uint32_t hostlong);
+//uint16_t htons(uint16_t hostshort);
+//uint32_t ntohl(uint32_t netlong);
+//uint16_t ntohs(uint16_t netshort);
 
 using namespace std;
 using namespace boost;
@@ -68,14 +73,14 @@ private:
 	boost::array<char, 1024> recv_buffer;
 };
 
-class SerializedSizeT
+class SizeTToken
 {
 	uint32_t data;
 
 public:
-	SerializedSizeT():data(0) {}
+	SizeTToken():data(0) {}
 
-	SerializedSizeT(size_t size)
+	SizeTToken(size_t size)
 	{
 		data = htonl((uint32_t)size);
 	}
@@ -85,197 +90,239 @@ public:
 		return (size_t)ntohl(data);
 	}
 
-	void* GetDataPtr() const
+	asio::mutable_buffers_1 GetAsioBuffer()
 	{
-		return (void*) &data;
+		return asio::buffer((void*) &data, sizeof(data));
 	}
 
-	static size_t DataSize()
+	asio::const_buffers_1 GetAsioBuffer() const
 	{
-		return sizeof(data);
+		return asio::buffer((const void*) &data, sizeof(data));
 	}
 };
 
-//class SerializableSizeT
-//{
-//	size_t size;
-//
-//public:
-//	explicit SerializableSizeT(void* source)
-//	{
-//		size = ntohl(*((uint32_t*) source));
-//	}
-//
-//	explicit SerializableSizeT(size_t size)
-//		:size(size) {}
-//
-//	size_t SerializedSize() const
-//	{
-//		return sizeof(uint32_t);
-//	}
-//
-//	void Serialize(void* target) const
-//	{
-//		uint32_t* targetAsInt = (uint32_t*) target;
-//		*targetAsInt = htonl((uint32_t)size); // careful 64-bit... if a message exceeds 32-bit we have a serious problem anyway.. but this is not strictly correct
-//	}
-//
-//	size_t GetValue() const
-//	{
-//		return size;
-//	}
-//};
-
-#define CHAT_BUFF_SIZE 16
-class session  // Fix async exceptions!
+class RawBuffer
 {
+	void *buffer;
+	size_t allocated_size;
+	size_t used_size;
+
+	RawBuffer(const RawBuffer& src)
+		:buffer(0),allocated_size(0),used_size(0) {}
+
 public:
-	//typedef void (completion_handler)(const boost::system::error_code& error, size_t size);
-
-	session(uint32_t id, io_service& service, unique_ptr<ip::tcp::socket> socket)
-		:id(id), service(service), socket(move(socket)) {}
-
-	void Setup(boost::function<void()> sessionReady)
+	explicit RawBuffer(size_t size)
+		:allocated_size(size), used_size(0)
 	{
-		SerializedSizeT requestMessageSize;
-		boost::asio::async_read(*socket, boost::asio::buffer(requestMessageSize.GetDataPtr(), SerializedSizeT::DataSize()), [this, sessionReady, reqSize](const boost::system::error_code& error, std::size_t size) {
-			requestMessageSize
+		buffer = ::operator new(allocated_size);
+	}
 
+	asio::const_buffers_1 GetAsioBuffer() const
+	{
+		return asio::buffer((const void*)buffer, used_size);
+	}
 
-		});
+//	asio::mutable_buffer GetAsioBuffer()
+//	{
+//		return asio::buffer(buffer, used_size);
+//	}
 
+	asio::mutable_buffers_1 ResetAndGetAsioBuffer(size_t size)
+	{
+		BOOST_ASSERT(size < allocated_size);
+		used_size = size;
+		return asio::buffer(buffer, used_size);
+	}
 
+	size_t GetSize() const
+	{
+		return allocated_size;
+	}
 
-		size_t reqSize = SessionRequest().ByteSize();
-		cout << "Asking for " << reqSize << endl;
-		BOOST_ASSERT(reqSize < chatBuffer.size());
+	size_t GetUsedSize() const
+	{
+		return used_size;
+	}
 
-		boost::asio::async_read(*socket, boost::asio::buffer(chatBuffer, reqSize), [this, sessionReady, reqSize](const boost::system::error_code& error, std::size_t size) {
+	~RawBuffer()
+	{
+		::operator delete(buffer);
+	}
+};
+
+class TCPTransceiver
+{
+	io_service& service;
+	unique_ptr<ip::tcp::socket> socket;
+
+public:
+	TCPTransceiver(io_service& service, unique_ptr<ip::tcp::socket> socket)
+		:service(service), socket(move(socket)) {}
+
+	void Send(RawBuffer& buffer, boost::function<void()> done)
+	{
+		auto messageSizeToken = new SizeTToken(buffer.GetUsedSize());
+		asio::async_write(*socket, messageSizeToken->GetAsioBuffer(), [this, done, messageSizeToken, &buffer](const system::error_code& error, size_t size) {
+			delete messageSizeToken;
+
 			if(error != 0)
-				throw "Error reading session request";
+				throw "Error sending message header";
 
-			if(size < reqSize)
-				throw "Received incomplete Session request";
-
-			auto req = new SessionRequest();
-			if(!req->ParsePartialFromArray(chatBuffer.elems, size))
-				throw "Received malformed Session request";
-
-			cout << "Got magick value from client " << req->magick() << endl;
-
-			SessionResponse response;
-			response.set_sessionid(id);
-			size_t responseSize = response.ByteSize();
-			BOOST_ASSERT(responseSize < chatBuffer.size());
-
-			bool succ = response.SerializeToArray(chatBuffer.elems, chatBuffer.size());
-			BOOST_ASSERT(succ);
-
-			boost::asio::async_write(*socket, boost::asio::buffer(chatBuffer, responseSize), [sessionReady, responseSize](const boost::system::error_code& error, size_t size) {
-				if(error != 0)
-					throw "Error writing session response!";
-
-				sessionReady();
+			asio::async_write(*socket, buffer.GetAsioBuffer(), [done](const system::error_code& error, size_t size) {
+				done();
 			});
 		});
 	}
 
-	uint32_t GetId() const
+	void Receive(RawBuffer& buffer, boost::function<void()> done)
 	{
-		return id;
+		auto messageSizeToken = new SizeTToken();
+		asio::async_read(*socket, messageSizeToken->GetAsioBuffer(), [this, done, messageSizeToken, &buffer](const system::error_code& error, size_t size) {
+			auto wrapped_token = unique_ptr<SizeTToken>(messageSizeToken);
+			if(error != 0)
+				throw "Error receiving message header";
+
+			size_t messageSize = wrapped_token->GetValue();
+			if(messageSize > buffer.GetSize())
+				throw "Error receiving message, message too large for buffer";
+
+			asio::async_read(*socket, buffer.ResetAndGetAsioBuffer(messageSize), [done](const system::error_code& error, size_t size) {
+				if(error != 0)
+					throw "Error receiving message header";
+
+				done();
+			});
+		});
 	}
-
-private:
-//	void hookup_async_read()
-//	{
-//		socket->async_read_some(boost::asio::buffer(*buffer, read_limit),
-//			boost::bind(&session::handle_async_read, this,
-//			boost::asio::placeholders::error,
-//			boost::asio::placeholders::bytes_transferred));
-//	}
-
-//	void handle_async_read(const boost::system::error_code& error, std::size_t size)
-//	{
-//		if(error)
-//		{
-//			cerr << "Error receiving data " << error.message() << endl;
-//			completion(error);
-//			return;
-//		}
-//
-//
-//		hookup_async_read();
-//	}
-
-	uint32_t id;
-	io_service& service;
-	unique_ptr<ip::tcp::socket> socket;
-	boost::array<char, CHAT_BUFF_SIZE> chatBuffer;
 };
 
-class listener
-{
-public:
-	//typedef void (new_session_handler)(session* new_session);
-
-	listener(boost::asio::io_service& service, short port)
-    	:service(service), acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), port)) {}
-
-	void set_new_session_handler(boost::function<void(session* new_session)> handler)
-	{
-		this->handler = handler;
-		hookup_async_accept();
-	}
-
-private:
-	void hookup_async_accept()
-	{
-		auto socket = new ip::tcp::socket(service);
-		acceptor.async_accept(*socket,
-			boost::bind(&listener::handle_accept, this, socket,
-			boost::asio::placeholders::error));
-	}
-
-	void handle_accept(ip::tcp::socket* socket, const boost::system::error_code& error)
-	{
-		if(error)
-		{
-	    	cerr << "Error accepting connection " + error.message() << endl;
-			hookup_async_accept();
-			return;
-		}
-
-		handler(new session(1, service, unique_ptr<ip::tcp::socket>(socket)));
-
-		//hookup_async_accept();
-	}
-
-private:
-	io_service& service;
-	ip::tcp::acceptor acceptor;
-	boost::function<void(session* new_session)> handler;
-};
-
-
-
-//class tcp_transceiver
+//#define CHAT_BUFF_SIZE 16
+//class session  // Fix async exceptions!
 //{
-//	tcp_transceiver(io_service& service)
-//		:service(service)
+//public:
+//	session(uint32_t id, io_service& service, unique_ptr<ip::tcp::socket> socket)
+//		:id(id), service(service), socket(move(socket)) {}
+//
+//	void Setup(boost::function<void()> sessionReady)
 //	{
-//		socket = ip::tcp::socket();
+//		SerializedSizeT requestMessageSize;
+//		boost::asio::async_read(*socket, boost::asio::buffer(requestMessageSize.GetDataPtr(), SerializedSizeT::DataSize()), [this, sessionReady, requestMessageSize](const boost::system::error_code& error, std::size_t size) {
+//			if(error != 0)
+//				throw "An error occured!";
+//
+//			if(size < SerializedSizeT::DataSize())
+//				throw "Received incomplete session request header";
+//
+//			size_t requestSize = requestMessageSize.GetValue();
+//			if(requestSize > chatBuffer.size())
+//				throw "Received too large session request";
+//
+//			boost::asio::async_read(*socket, boost::asio::buffer(chatBuffer, requestSize), [this, sessionReady, requestSize](const boost::system::error_code& error, std::size_t size) {
+//				if(error != 0)
+//					throw "Error reading session request";
+//
+//				if(size < requestSize) // Is this possible when there is no error?
+//					throw "Received incomplete Session request";
+//
+//				auto req = new SessionRequest();
+//				if(!req->ParseFromArray(chatBuffer.elems, size))
+//					throw "Received malformed Session request";
+//
+//				cout << "Got magick value from client " << req->magick() << endl;
+//
+//				SessionResponse response;
+//				response.set_sessionid(id);
+//				size_t responseSize = response.ByteSize();
+//				BOOST_ASSERT(responseSize < chatBuffer.size());
+//
+//				bool succ = response.SerializeToArray(chatBuffer.elems, chatBuffer.size());
+//				BOOST_ASSERT(succ);
+//
+//				boost::asio::async_write(*socket, boost::asio::buffer(chatBuffer, responseSize), [sessionReady, responseSize](const boost::system::error_code& error, size_t size) {
+//					if(error != 0)
+//						throw "Error writing session response!";
+//
+//					sessionReady();
+//				});
+//			});
+//		});
+//	}
+//
+//	uint32_t GetId() const
+//	{
+//		return id;
 //	}
 //
 //private:
-//	boost::asio::io_service& service;
-//	ip::udp::socket *socket;
+////	void hookup_async_read()
+////	{
+////		socket->async_read_some(boost::asio::buffer(*buffer, read_limit),
+////			boost::bind(&session::handle_async_read, this,
+////			boost::asio::placeholders::error,
+////			boost::asio::placeholders::bytes_transferred));
+////	}
 //
-//	ip::udp::endpoint remote_endpoint_;
-//	boost::array<char, 1024> recv_buffer;
+////	void handle_async_read(const boost::system::error_code& error, std::size_t size)
+////	{
+////		if(error)
+////		{
+////			cerr << "Error receiving data " << error.message() << endl;
+////			completion(error);
+////			return;
+////		}
+////
+////
+////		hookup_async_read();
+////	}
+//
+//	uint32_t id;
+//	io_service& service;
+//	unique_ptr<ip::tcp::socket> socket;
+//	boost::array<char, CHAT_BUFF_SIZE> chatBuffer;
 //};
-
-
-
+//
+//class listener
+//{
+//public:
+//	//typedef void (new_session_handler)(session* new_session);
+//
+//	listener(boost::asio::io_service& service, short port)
+//    	:service(service), acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), port)) {}
+//
+//	void set_new_session_handler(boost::function<void(session* new_session)> handler)
+//	{
+//		this->handler = handler;
+//		hookup_async_accept();
+//	}
+//
+//private:
+//	void hookup_async_accept()
+//	{
+//		auto socket = new ip::tcp::socket(service);
+//		acceptor.async_accept(*socket,
+//			boost::bind(&listener::handle_accept, this, socket,
+//			boost::asio::placeholders::error));
+//	}
+//
+//	void handle_accept(ip::tcp::socket* socket, const boost::system::error_code& error)
+//	{
+//		if(error)
+//		{
+//	    	cerr << "Error accepting connection " + error.message() << endl;
+//			hookup_async_accept();
+//			return;
+//		}
+//
+//		handler(new session(1, service, unique_ptr<ip::tcp::socket>(socket)));
+//
+//		//hookup_async_accept();
+//	}
+//
+//private:
+//	io_service& service;
+//	ip::tcp::acceptor acceptor;
+//	boost::function<void(session* new_session)> handler;
+//};
 
 //int call_b(boost::function<int(int)> fnc, int x)
 //{
@@ -309,38 +356,18 @@ int main()
 	//cout << call_b(add_h, 2) << endl;
 
 
-	io_service service;
-	listener listen(service, 555);
-
-	//boost::function<void(session*)> session_h = [](session* nsession) {};
-
-	//listen.set_new_session_handler(&session_h);
-	//listen.set_new_session_handler(boost::function<void(session*)>([&](session *nsession) { }));
-
-	//boost::array<char, BUFF_SIZE> buffer;
-
-	//boost::asio::streambuf buffer;
-	//boost::asio::buffer bf; //(chars, 1024); //b.prepare(1024)
-	//auto bf = boost::asio::buffer(b.prepare(1024));
-
-	listen.set_new_session_handler([](session* nsession) {
-		cout << "New session accepted!" << endl;
-		nsession->Setup([nsession]() {
-			cout << "Session set up. (id: " << nsession->GetId() << ")" << endl;
-		});
-	});
-
-	service.run();
-
+//	io_service service;
+//	listener listen(service, 555);
 //
-//	stringstream ss;
+//	listen.set_new_session_handler([](session* nsession) {
+//		cout << "New session accepted!" << endl;
+//		nsession->Setup([nsession]() {
+//			cout << "Session set up. (id: " << nsession->GetId() << ")" << endl;
+//		});
+//	});
 //
-// 	ss << "Hahahahhahah";
-//
-// 	string s = ss.str();
-//
-// 	cout << s << endl;
-//
+//	service.run();
+
 //	udp_transceiver transceiver(service);
 //
 //	cout << "Server running:" << endl;
